7985a93bc41af81af1efe86ef2ae5890dab957bc
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2026-05-08 23:54:50 -0300

    Create GridGameApplication.java

---
+++ GridGame/src/main/java/com/game/GridGame/GridGameApplication.java
@@ -0,0 +1,14 @@
+package com.game.GridGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GridGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GridGameApplication.class, args);
+	}
+
+}
+	
