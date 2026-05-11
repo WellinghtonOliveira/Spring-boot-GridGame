2a03cc50cc8ae8065e64d45903d664eccc31592d
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2026-05-11 19:47:09 -0300

    Create GridGameApplicationTests.java

---
+++ GridGame/src/test/java/com/game/GridGame/GridGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game.GridGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GridGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
