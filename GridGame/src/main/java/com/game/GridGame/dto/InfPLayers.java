4d2349e396702490cea1b823b9bdbb8bb1c92a2e
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2026-05-17 22:05:26 -0300

    Create InfPLayers.java

---
+++ GridGame/src/main/java/com/game/GridGame/dto/InfPLayers.java
@@ -0,0 +1,22 @@
+package com.game.GridGame.dto;
+
+public class InfPLayers {
+    private String id;
+    private String direcao;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDirecao() {
+        return direcao;
+    }
+
+    public void setDirecao(String direcao) {
+        this.direcao = direcao;
+    }
+}
