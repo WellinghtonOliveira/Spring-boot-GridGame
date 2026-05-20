018715fef3ad9023ebe655dd8e0f83de4b91f7a1
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2026-05-19 22:05:10 -0300

    Update ServiceJogador.java

--- GridGame/src/main/java/com/game/GridGame/service/ServiceJogador.java
+++ GridGame/src/main/java/com/game/GridGame/service/ServiceJogador.java
@@ -60,21 +60,18 @@ public class ServiceJogador {
 
     public void observerPlayer(String id) {
         JogadorEntity jogador = jogadores.get(id);
-
-        System.out.println(id);
+        
         if (jogador == null) {
             return;
         }
 
         if (jogador.getY() / 40 >= mapaService.obterAltura() - 1) {
             jogador.setY((mapaService.obterAltura() - 1) * 40);
-            System.out.println("ID recebido: " + id);
-            System.out.println("Jogadores: " + jogadores.keySet());
         }
 
         observerCanJump(jogador);
         boolean noChao = observerColisionFoot(jogador);
-
+        
         if (!noChao && jogador.getEmpuxo()) {
             if (colisaoPosPuloFoot(jogador)) {
                 int yBloco = ((int) ((jogador.getY() + 39 + jogador.getGravity()) / 40)) * 40;
@@ -114,7 +111,6 @@ public class ServiceJogador {
         String[][] mapa = mapaService.obterMapa(null);
 
         int x = (int) (jogador.getX() / 40);
-        int y = (int) (jogador.getY() / 40);
 
         int xDireita = (int) ((jogador.getX() + 40) / 40);
         int xEsquerda = (int) ((jogador.getX() - 1) / 40);
@@ -123,8 +119,7 @@ public class ServiceJogador {
         int yBaixo = (int) ((jogador.getY() + 39) / 40);
 
         if (x >= 0 &&
-                y + 1 < mapaService.obterAltura() &&
-                x < mapaService.obterLargura()) {
+            x < mapaService.obterLargura()) {
 
             if (direction.equals("direita") &&
                     mapa[yCima][xDireita].equals("vazio") &&
@@ -142,20 +137,11 @@ public class ServiceJogador {
     public Boolean observerColisionHead(JogadorEntity jogador) {
         String[][] mapa = mapaService.obterMapa(null);
 
-        int alturaMapa = mapaService.obterAltura();
-
-        int x = (int) jogador.getX() / 40;
-        int y = (int) (jogador.getY() / 40);
-
         int xEsquerda = (int) ((jogador.getX() + 1) / 40);
         int xDireita = (int) ((jogador.getX() + 39) / 40);
         int yCima = (int) ((jogador.getY() - 1) / 40);
 
-        if (y + 1 < alturaMapa &&
-                y - 1 > 0 &&
-                x >= 0 &&
-                x < mapaService.obterLargura()) {
-
+        if (((jogador.getY() - 1) / 40) >= 0) {
             if (mapa[yCima][xEsquerda].equals("vazio") &&
                     mapa[yCima][xDireita].equals("vazio")) {
                 return true;
@@ -168,15 +154,13 @@ public class ServiceJogador {
         String[][] mapa = mapaService.obterMapa(null);
 
         int x = (int) jogador.getX() / 40;
-        int y = (int) (jogador.getY() / 40);
 
-        int xEsquerda = (int) ((jogador.getX() + 1) / 40);
-        int xDireita = (int) ((jogador.getX() + 39) / 40);
+        int xEsquerda = (int) ((jogador.getX() + 5) / 40);
+        int xDireita = (int) ((jogador.getX() + 35) / 40);
         int yAbaixo = (int) ((jogador.getY() + 40) / 40);
 
-        if (y + 1 < mapaService.obterAltura() &&
-                x >= 0 &&
-                x < mapaService.obterLargura()) {
+        if (x >= 0 &&
+            x < mapaService.obterLargura()) {
 
             if (mapa[yAbaixo][xEsquerda].equals("chao") ||
                     mapa[yAbaixo][xDireita].equals("chao")) {
@@ -212,6 +196,7 @@ public class ServiceJogador {
         if (jogador.getGravity() < 15) {
             jogador.setGravity(jogador.getGravity() + 1);
         }
+
     }
 
     public JogadorEntity criarJogador() {
@@ -220,8 +205,3 @@ public class ServiceJogador {
         return jogador;
     }
 }
-
-// TODO correções
-// player: muito largo
-// ao pular de um bloco de baixo para cima em uma abertura de 1 bloco o player e
-// lancado para cima, verificar as colisoes
