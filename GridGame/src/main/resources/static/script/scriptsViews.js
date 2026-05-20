ed223e0743f27bf7b969d94ce8c458b12a3b2391
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2026-05-20 14:38:00 -0300

    Update scriptsViews.js

--- GridGame/src/main/resources/static/script/scriptsViews.js
+++ GridGame/src/main/resources/static/script/scriptsViews.js
@@ -8,10 +8,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
     buttoncomecar.addEventListener("click", () => {
         const containerEscolhas = document.getElementById("container-criacao");
+        const camera = document.getElementById("camera");
         const game = document.getElementById("game");
         containerEscolhas.style.display = "none";
         game.style.display = "grid";
         window.jogadorOn = true;
+
+
+        camera.style.width = "100vw";
+        camera.style.height = "100vh";
+        camera.style.overflow = "hidden";
+        camera.style.position = "relative";
     })
 
 })
