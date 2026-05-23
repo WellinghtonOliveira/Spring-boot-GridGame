0e5ab3c3a1ef8779521a3e28870423f1132c0a94
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2026-05-22 21:48:37 -0300

    Update scriptsViews.js

--- GridGame/src/main/resources/static/script/scriptsViews.js
+++ GridGame/src/main/resources/static/script/scriptsViews.js
@@ -1,3 +1,5 @@
+import { jogador, confJogadorInit } from "./scripts.js";
+
 document.addEventListener("DOMContentLoaded", () => {
     const buttoncomecar = document.getElementById("button-comecar");
     const buttonCriarMapa = document.getElementById("button-criar-mapa");
@@ -6,19 +8,35 @@ document.addEventListener("DOMContentLoaded", () => {
         window.location.href = "./pages/criacao/index.html"
     })
 
-    buttoncomecar.addEventListener("click", () => {
-        const containerEscolhas = document.getElementById("container-criacao");
-        const camera = document.getElementById("camera");
-        const game = document.getElementById("game");
-        containerEscolhas.style.display = "none";
-        game.style.display = "grid";
-        window.jogadorOn = true;
+    buttoncomecar.addEventListener("click", async () => {
+        if (configName() && configCor()) {
+            await confJogadorInit();
 
+            const containerEscolhas = document.getElementById("container-criacao");
+            const game = document.getElementById("game");
 
-        camera.style.width = "100vw";
-        camera.style.height = "100vh";
-        camera.style.overflow = "hidden";
-        camera.style.position = "relative";
+            containerEscolhas.style.display = "none";
+            game.style.display = "grid";
+            window.jogadorOn = true;
+        }
     })
 
 })
+
+function configCor() {
+    let cor = document.getElementById("input-color");
+    if (cor) {
+        jogador.cor = cor.value;
+        return true;
+    }
+    return false;
+}
+
+function configName() {
+    let nome = document.getElementById("input-text-nome");
+    if (nome && nome.value < 3) {
+        jogador.nome = nome;
+        return true;
+    }
+    return false;
+}
