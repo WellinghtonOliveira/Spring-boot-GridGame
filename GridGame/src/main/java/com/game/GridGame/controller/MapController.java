a519bd709ea0ed67462c01076ee4bb5b3e550321
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2026-06-08 18:44:06 -0300

    Update MapController.java

--- GridGame/src/main/java/com/game/GridGame/controller/MapController.java
+++ GridGame/src/main/java/com/game/GridGame/controller/MapController.java
@@ -1,5 +1,7 @@
 package com.game.GridGame.controller;
 
+import java.util.Map;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +16,11 @@ import com.game.GridGame.service.ServiceMap;
 @RequestMapping("/maps")
 public class MapController {
     private ServiceMap serviceMaps = new ServiceMap();
+
+    @GetMapping
+    public ResponseEntity<Map<Integer, String[][]>> obtemTodosMapas() {
+        return ResponseEntity.ok(serviceMaps.obtemTodosMapas());
+    }
 
     @GetMapping("/{idMapa}")
     public String[][] obtemMap(@PathVariable Integer idMapa) {
