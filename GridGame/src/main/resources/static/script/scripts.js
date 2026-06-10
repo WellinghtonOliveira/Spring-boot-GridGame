ea15e7f10545945544bb6ff0b7f05179c2e35b87
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2026-06-10 20:00:02 -0300

    Update scripts.js

--- GridGame/src/main/resources/static/script/scripts.js
+++ GridGame/src/main/resources/static/script/scripts.js
@@ -1,15 +1,20 @@
 const url = "http://localhost:8080/";
 
+let idMapa = 0;
+
 export let jogador = {
     "id": "",
+    "idMapa": idMapa,
     "nome": "PLAYER",
     "cor": "#e0e0e0"
 }
 
+const displayMinimapa = document.getElementById("display-grid-mapa");
+
 let grid;
 let tela;
 let game;
-let MAPAS;
+let MAPAS = [];
 
 let cameraX = 0;
 let cameraY = 0;
@@ -28,10 +33,23 @@ document.addEventListener("DOMContentLoaded", () => {
         await carregarMapas();
         desenharGrid();
         desenharBlock();
+        selecaoMapa();
     }
 
     init();
 })
+
+function selecaoMapa() {
+    const select = document.getElementById("select-mapas");
+
+    select.addEventListener("change", () => {
+        idMapa = select.value;
+        jogador.idMapa = idMapa;
+
+        desenharGrid();
+        desenharBlock();
+    });
+}
 
 export async function confJogadorInit() {
     await geraJogador();
@@ -234,15 +252,19 @@ function atualizarCamera(jogador) {
 }
 
 function desenharBlock() {
-    const rows = MAPAS.length;
-    const cols = MAPAS[0].length;
+    const rows = MAPAS[idMapa].length;
+    const cols = MAPAS[idMapa][0].length;
 
     for (let y = 0; y < rows; y++) {
         for (let x = 0; x < cols; x++) {
 
-            if (MAPAS[y][x] == "chao") {
+            if (MAPAS[idMapa][y][x] == "chao") {
                 const index = y * cols + x;
                 const cell = document.getElementById(`cell-num-${index}`);
+                const cellMinimapa = document.getElementById(`cell-num-${index}-minimapa`);
+
+                cellMinimapa.style.backgroundColor = '#664300';
+                cellMinimapa.style.border = 'none';
 
                 cell.style.backgroundColor = '#664300';
                 cell.style.border = 'none';
@@ -252,8 +274,14 @@ function desenharBlock() {
 }
 
 function desenharGrid() {
-    const rows = MAPAS.length;
-    const cols = MAPAS[0].length;
+
+    const rows = MAPAS[idMapa].length;
+    const cols = MAPAS[idMapa][0].length;
+
+    displayMinimapa.style.gridTemplateColumns = `repeat(${cols}, 10px)`;
+    displayMinimapa.style.gridTemplateRows = `repeat(${rows}, 10px)`;
+
+    displayMinimapa.innerHTML = '';
 
     grid.style.gridTemplateColumns = `repeat(${cols}, 40px)`;
     grid.style.gridTemplateRows = `repeat(${rows}, 40px)`;
@@ -262,28 +290,50 @@ function desenharGrid() {
 
     for (let i = 0; i < cols * rows; i++) {
         const cell = document.createElement('div');
+        const cellMinimapa = document.createElement('div');
+
         const x = i % cols;
         const y = Math.floor(i / cols);
 
         if (y === 0) {
+            cellMinimapa.style.borderTop = "1px solid rgba(255,255,255,0.3)";
             cell.style.borderTop = "1px solid rgba(255,255,255,0.3)";
         }
 
         if (x === 0) {
+            cellMinimapa.style.borderLeft = "1px solid rgba(255,255,255,0.3)";
             cell.style.borderLeft = "1px solid rgba(255,255,255,0.3)";
         }
 
+        cellMinimapa.id = `cell-num-${i}-minimapa`;
+        cellMinimapa.classList.add("cellMinimapa");
+        displayMinimapa.appendChild(cellMinimapa);
+
         cell.id = `cell-num-${i}`;
         cell.classList.add("cell");
-        grid.appendChild(cell)
+        grid.appendChild(cell);
     }
 }
 
 async function carregarMapas() {
-    try {
 
-        const input = await fetch(url + "maps");
+    const selectMapas = document.getElementById("select-mapas");
+
+    try {
+        const input = await fetch(url + `maps`);
         MAPAS = await input.json();
+
+        let quantidade = Object.keys(MAPAS).length;
+
+        if (quantidade > 1) {
+            for (let i = 1; i < quantidade; i++) {
+                const op = document.createElement("option");
+                op.value = i;
+                op.textContent = `MAPA ${i}`;
+
+                selectMapas.appendChild(op);
+            }
+        }
 
     } catch (error) {
         console.log("Erro: " + error);
