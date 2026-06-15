0e7e8161b8a917b456385068f18b44fb68a0d121
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2026-06-14 21:56:32 -0300

    Create ResponsePlayerId.java

---
+++ GridGame/src/main/java/com/game/GridGame/dto/ResponsePlayerId.java
@@ -0,0 +1,18 @@
+package com.game.GridGame.dto;
+
+public class ResponsePlayerId {
+    private String id;
+
+    public ResponsePlayerId(String id) {
+        this.id = id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+    
+}
