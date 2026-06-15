6566a0acb220b90639ff43c58bcb21be45c3f9c7
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2026-06-15 19:05:11 -0300

    Update GridGameApplication.java

--- GridGame/src/main/java/com/game/GridGame/GridGameApplication.java
+++ GridGame/src/main/java/com/game/GridGame/GridGameApplication.java
@@ -7,9 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class GridGameApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GridGameApplication.class, args);
 	}
- 
 }
