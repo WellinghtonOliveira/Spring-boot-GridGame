2fa493f722e7281518f7de4012acf2d59e143c53
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2026-06-30 08:11:11 -0300

    Update ServiceJogador.java

--- GridGame/src/main/java/com/game/GridGame/service/ServiceJogador.java
+++ GridGame/src/main/java/com/game/GridGame/service/ServiceJogador.java
@@ -86,6 +86,7 @@ public class ServiceJogador {
         if (jogador == null) return;
         
         observerCanJump(jogador);
+        observerColisionFacesDamage(jogador);
         boolean noChao = observerColisionFoot(jogador);
         
         
@@ -141,12 +142,12 @@ public class ServiceJogador {
 
         if (x >= 0) {
             if (direction.equals("direita") && xDireita < mapaService.obterLargura(jogador.getIdMapa()) &&
-                    mapa[yCima][xDireita].equals("vazio") &&
-                    mapa[yBaixo][xDireita].equals("vazio")) {
+                mapa[yCima][xDireita].equals("vazio") &&
+                mapa[yBaixo][xDireita].equals("vazio")) {
                 return true;
             } else if (direction.equals("esquerda") &&
-                    mapa[yCima][xEsquerda].equals("vazio") &&
-                    mapa[yBaixo][xEsquerda].equals("vazio")) {
+                mapa[yCima][xEsquerda].equals("vazio") &&
+                mapa[yBaixo][xEsquerda].equals("vazio")) {
                 return true;
             }
         }
@@ -162,7 +163,7 @@ public class ServiceJogador {
 
         if (((jogador.getY() - 1) / 40) >= 0) {
             if (mapa[yCima][xEsquerda].equals("vazio") &&
-                    mapa[yCima][xDireita].equals("vazio")) {
+                mapa[yCima][xDireita].equals("vazio")) {
                 return true;
             }
         }
@@ -182,7 +183,6 @@ public class ServiceJogador {
 
         if (x >= 0 &&
             x < mapaService.obterLargura(jogador.getIdMapa())) {
-
             if (mapa[yAbaixo][xEsquerda].equals("chao") ||
                 mapa[yAbaixo][xDireita].equals("chao")) {
                 return true;
@@ -255,6 +255,18 @@ public class ServiceJogador {
         if (jogador != null) {
             jogador.setUltimoPing(System.currentTimeMillis());
         }
+    }
+
+    public void observerColisionFacesDamage(JogadorEntity jogador) {
+        String[][] mapa = mapaService.obterMapa(jogador.getIdMapa());
+
+        int x = (int) jogador.getX() / 40;
+
+        int xEsquerda = (int) ((jogador.getX() + 5) / 40);
+        int xDireita = (int) ((jogador.getX() + 35) / 40);
+        int yAbaixo = (int) ((jogador.getY() + 40) / 40);
+        int yCima = (int) ((jogador.getY() - 1) / 40);
+
     }
 
     public ResponseEntity<String> updatePlayer(InfPLayers data) {
